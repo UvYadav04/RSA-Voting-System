7f5dfcd1133bf24d647fbe3a74183f1f239c667f
Dinesh Yadav <dineshnirban01@gmail.com>
2024-12-05 08:59:26 +0530

    update to git

--- index.js
+++ index.js
@@ -210,7 +210,7 @@ const privateKeyserver = fs.readFileSync('private.key', 'utf8');
 const certificate = fs.readFileSync('mydomain.crt', 'utf8');
 const credentials = { key: privateKeyserver, cert: certificate };
 
-https.createServer(credentials, app).listen(port, '0.0.0.0', () => {
+https.createServer(credentials, app).listen(port, () => {
     console.log('HTTPS server running on port 8080');
 });
 
